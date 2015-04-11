ee8d86fbbd17347dfb25baa7d3c7b392701e8325
Vera Sazonova <verasazonova@gmail.com>
2015-04-11 16:24:22 -0400

    Changed the name of get_twits to get_tweets for consistency.  Removed various PEP errors

--- tweet_mining/get_tweets.py
+++ tweet_mining/get_tweets.py
@@ -14,6 +14,22 @@ CONSUMER_SECRET = "Rbl49F5C4Fbbwt7maZBw3IB325Q1XMa8xRQ4I54UerJXxFKQSS"
 OAUTH_TOKEN = "277586033-34CoMaWtywqgC37tc6CgDKJhOHDZUfTt1kwHKJay"
 OAUTH_TOKEN_SECRET = "6cvYDqYFAlCsw3WETSZ6MFb5C3GB4qe6BVT78Ua33fNPq"
 
+
+def oauth_login():
+    auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+    return twitter.Twitter(auth=auth)
+
+
+def save_json(filename, data):
+    with codecs.open(filename, 'w', encoding='utf-8') as f:
+        f.write(unicode(json.dumps(data, ensure_ascii=False)))
+
+
+def load_json(filename):
+    with codecs.open(filename, encoding='utf-8') as f:
+        return f.read()
+
+
 fields_str = "text,retweet_count,favorited,truncated,id_str,in_reply_to_screen_name,source,retweeted,created_at," \
              "in_reply_to_status_id_str,in_reply_to_user_id_str,lang,listed_count,verified,location,user_id_str," \
              "description,geo_enabled,user_created_at,statuses_count,followers_count,favourites_count,protected," \
@@ -28,50 +44,44 @@ def normalize_format(phrase):
     return norm_phrase
 
 
-def oauth_login():
-    auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,
-                               CONSUMER_KEY, CONSUMER_SECRET)
-
-    return twitter.Twitter(auth=auth)
-
-
-def save_json(filename, data):
-    with codecs.open(filename, 'w', encoding='utf-8') as f:
-        f.write(unicode(json.dumps(data, ensure_ascii=False)))
-
-def load_json(filename):
-    with codecs.open(filename, encoding='utf-8') as f:
-        return f.read()
-
-
-def save_tweet(twit, filename):
+def save_tweet(tweet, filename):
+    # save the fields in the tweet defined by the fields list.  Append to a given file
     with codecs.open(filename, 'a', encoding='utf-8') as fout:
         for field in fields:
-            if twit.has_key(field):
-                fout.write(",%s" % twit[field])
+            if tweet in fields:
+                fout.write(",%s" % tweet[field])
             else:
                 fout.write(",NA")
         fout.write("\n")
 
 
 def search_tweets(q, max_id=None, since_id=None):
+    """
 
+    Searches for older tweets defined by a search parameter q, max_id, since_id
+
+    :param q: Search parameter
+    :param max_id: the latest (max) id_str of the tweet
+    :param since_id: the earliest id_str of the tweet
+    :return: the oldest id_str found within the 100x10 tweets (100 tweets per page, 10 pages)
+    """
     twitter_api = oauth_login()
 
     count = 100
+    # set up the query for a search, depending whether max_id and or since_id are provided
     if max_id is not None:
         if since_id is not None:
             if since_id >= max_id:
                 print max_id, since_id, "Since id later than max id"
                 return None
             search_results = twitter_api.search.tweets(q=q, count=count, result_type='recent', max_id=max_id,
-                                                   since_id=since_id)
+                                                       since_id=since_id)
         else:
             search_results = twitter_api.search.tweets(q=q, count=count, result_type='recent', max_id=max_id)
     else:
         search_results = twitter_api.search.tweets(q=q, count=count, result_type='recent')
 
-    print "Searching for %s with max_id of %s" % ( q, max_id)
+    print "Searching for %s with max_id of %s" % (q, max_id)
 
     try:
         statuses = search_results['statuses']
@@ -81,8 +91,8 @@ def search_tweets(q, max_id=None, since_id=None):
             print "Length of statuses", len(statuses)
             try:
                 next_results = search_results['search_metadata']['next_results']
-            except KeyError, e: # No more results when next_results doesn't exist
-                print "no more tweets"
+            except KeyError, e:  # No more results when next_results doesn't exist
+                print "No more tweets %s" % e
                 break
 
             # Create a dictionary from next_results, which has the following form:
@@ -92,11 +102,13 @@ def search_tweets(q, max_id=None, since_id=None):
             search_results = twitter_api.search.tweets(**kwargs)
             statuses += search_results['statuses']
 
+        # search will be saved into a file named by query phrase, max_id and since_id
         filename = "%s_search_%s_%s.txt" % (q, max_id, since_id)
         print "Saving to %s" % filename
 
         cnt = 0
         oldest_id = ""
+        # save tweets and find the oldest tweet id.
         for status in statuses:
             save_tweet(status, filename)
             if cnt == 0:
@@ -108,13 +120,19 @@ def search_tweets(q, max_id=None, since_id=None):
         return oldest_id
 
     except KeyError, e:
-        print "No tweets found"
+        print "No tweets found %s" % e
         return None
 
 
 def connect_stream(q):
+    """
+    Connects to a twitter stream defined by a query phrase and saves it to a file
 
-    # Connect to a stream.
+    :param q: search phrase
+    :return: nothing
+    """
+
+    # Connect to a stream defined by the query phrase 1
     twitter_api = oauth_login()
     twitter_stream = twitter.TwitterStream(auth=twitter_api.auth)
     stream = twitter_stream.statuses.filter(track=q)
